ce398b9403a72e12543f532cd729318a8dab28ab
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-08 21:18:06 +0800

    Enclosed the require_once parameter

--- reset.php
+++ reset.php
@@ -9,7 +9,7 @@
 	}
 	 
 	// Include config file
-	require_once "config.php";
+	require_once ("config.php");
 	 
 	// Define variables and initialize with empty values
 	$new_password = $confirm_password = "";
